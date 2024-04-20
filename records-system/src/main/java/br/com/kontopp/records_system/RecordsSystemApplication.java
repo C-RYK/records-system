7040ec1a1846439e68c11fd79c611d342cc049b6
Carlos Henry Jr. <carlo.henrique37@gmail.com>
2024-04-20 19:02:25 -0300

    Subindo arquivos base

---
+++ records-system/src/main/java/br/com/kontopp/records_system/RecordsSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.kontopp.records_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordsSystemApplication.class, args);
+	}
+
+}
---
+++ records-system/src/test/java/br/com/kontopp/records_system/RecordsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kontopp.records_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
